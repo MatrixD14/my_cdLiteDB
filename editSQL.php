ec70ce06ef32386a778ff2a1f0cb7cdec42905de
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2025-09-11 19:47:35 -0400

    Update editSQL.php
    agora exporta em json e csv os dados

--- editSQL.php
+++ editSQL.php
@@ -2,6 +2,12 @@
 ob_start();
 session_start();
 function r(){header("Location:index.php");exit();};
+function table($ro,$n=''){
+  if(!$ro)return;
+  echo "<br><br>$n<hr><div style='max-height:400px;overflow:auto;'><table border=2><tr><th>".implode("</th><th>",array_keys($ro[0]))."</th></tr>";
+  foreach($ro as $v) echo"<tr><td>".implode("</td><td>",$v)."</td></tr>";
+   echo"</table></div>";
+}
 if(!($f=$_POST['file']??($_GET['file']??''))) r();
 if(!($_SESSION['logo']??0))if(isset($_POST['us'], $_POST['pass'])&&$_POST['us']=="admin"&&$_POST['pass']=="admin"){
        $_SESSION['logo']=1;
@@ -11,10 +17,33 @@ try{
     ($db=new PDO("sqlite:$f"))->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 }catch(Exception $e){r();}
 $tb=$db->query("SELECT name FROM sqlite_master WHERE type='table'")->fetchAll(PDO::FETCH_COLUMN);
+if(isset($_GET['export'])&&($t=$_GET['export'])){
+  if(in_array($t,$tb)){
+  $data=$db->query("SELECT * FROM $t")->fetchAll(PDO::FETCH_ASSOC);
+  if(($_GET['type']??'')=='json'){
+  header('Content-Type: application/json; charset=utf-8');
+  header('Content-Disposition: attachment; filename="'.$t.'.json"');
+  echo json_encode($data, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+  exit;
+  }else{
+    header('Content-Type: text/csv; charset=utf-8');
+        header('Content-Disposition: attachment; filename="'.$t.'.csv"');
+        $fout=fopen('php://output','w');
+        if(!empty($data)){
+            fputcsv($fout,array_keys($data[0]),",",'"',"\\");
+            foreach($data as $row) fputcsv($fout,$row,",",'"',"\\");
+        }
+        fclose($fout);
+    exit;
+  }
+  }
+}
 foreach($tb as $t)if($t!='sqlite_sequence')echo"<br><a href=?file=$f&t=$t>$t</a><hr>";
 if(($t=$_GET['t']??$_SESSION['lab']??'') && in_array($t,$tb)){
   try{
-  echo($ro=$db->query("SELECT * FROM $t")->fetchAll(PDO::FETCH_ASSOC))?table($ro,$t):table($db->query("PRAGMA table_info($t)")->fetchAll(PDO::FETCH_ASSOC),$t);}catch(Exception $e){echo'erro: '.$e->getMessage();}
+  echo($ro=$db->query("SELECT * FROM $t")->fetchAll(PDO::FETCH_ASSOC))?table($ro,$t):table($db->query("PRAGMA table_info($t)")->fetchAll(PDO::FETCH_ASSOC),$t);
+    echo"<hr><a href=\"?file=$f&export=$t&type=json\">ExportJSON</a><br><br><a href=\"?file=$f&export=$t&type=csc\">ExportCSV</a>";
+  }catch(Exception $e){echo'erro: '.$e->getMessage();}
 }
 if(!empty($_POST['CMD'])){
   $_SESSION['cmd']=($sql=$_POST['CMD']);
@@ -28,14 +57,7 @@ if(!empty($_POST['CMD'])){
   else{header("Location: ?file=$f&run=1");
   exit;}
 }
-function table($ro,$n=''){
-  if(!$ro)return;
-  echo "<br>$n<div style='max-height:400px; overflow:auto;'><table border=2><tr><th>".implode("</th><th>",array_keys($ro[0]))."</th></tr>";
-  foreach($ro as $v) echo"<tr><td>".implode("</td><td>",$v)."</td></tr>";
-   echo"</table></div>";
-}
 if(isset($_GET['LIMP']))$_SESSION['cmd']='';
-ob_end_flush();
 ?>
 <br><br><form method="post" onsubmit="return check()">
         <textarea name="CMD"id="CMD"rows="10" cols="50" placeholder="Digite seu command SQL..." required><?=htmlspecialchars($_SESSION['cmd']??'')?></textarea><br>
@@ -52,4 +74,5 @@ ob_end_flush();
 <?php
 if(!empty($_SESSION['history']))
   foreach(array_slice(array_reverse($_SESSION['history']),-1000) as $hi)echo"$hi\n\n";
+  ob_end_flush();
 ?></textarea>
